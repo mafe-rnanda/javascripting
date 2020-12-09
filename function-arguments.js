e2b361429fd7dc93c2e868d10bd0203e9960474f
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2020-12-09 00:32:15 +0000

    working with objects and functions

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(numOne,numTwo,numThree) {
+  return (numTwo * numThree) + numOne;
+}
+
+console.log(math(53, 61, 67));
---
+++ object-keys.js
@@ -0,0 +1,8 @@
+const car = {
+  make: 'Honda',
+  model: 'Accor',
+  year: 2020
+}
+
+const keys = Object.keys(car);
+console.log(keys);
